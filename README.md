c1be915825ce861af789fa2ca6099e4f544ce6cf
snowinszu <86755838@qq.com>
2023-07-07 15:42:59 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SwiftNATTypeDetector
+
+A description of this package.
---
+++ Sources/SwiftNATTypeDetector/SwiftNATTypeDetector.swift
@@ -0,0 +1,6 @@
+public struct SwiftNATTypeDetector {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
